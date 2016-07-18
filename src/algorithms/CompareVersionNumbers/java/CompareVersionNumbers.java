9494cd22af0d28855e69894f5afc16e1e4efea32
fisbii <fisher.bi@leandev.se>
2016-07-18 16:45:53 +0800

    Compare version numbers

---
+++ src/algorithms/CompareVersionNumbers/java/CompareVersionNumbers.java
@@ -0,0 +1,11 @@
+package algorithms.CompareVersionNumbers.java;
+
+/**
+ * Created by fisbii on 16-7-18.
+ */
+public class CompareVersionNumbers {
+
+    public int compareVersion(String version1, String version2) {
+        return 1;
+    }
+}

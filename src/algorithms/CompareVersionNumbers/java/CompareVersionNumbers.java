9e6cc1a6fb5b5c52c5ec9778e9764c13c50a812e
fisbii <fisher.bi@leandev.se>
2016-07-18 17:19:43 +0800

    Compare version numbers

--- src/algorithms/CompareVersionNumbers/java/CompareVersionNumbers.java
+++ src/algorithms/CompareVersionNumbers/java/CompareVersionNumbers.java
@@ -6,6 +6,32 @@ package algorithms.CompareVersionNumbers.java;
 public class CompareVersionNumbers {
 
     public int compareVersion(String version1, String version2) {
-        return 1;
+        String v1[] = version1.split("\\.");
+        String v2[] = version2.split("\\.");
+        int length = Math.max(v1.length,v2.length);
+        for(int i = 0; i < length; i++){
+            if(i >= v1.length){
+                for(int j = i; j < length; j++){
+                    if(Integer.parseInt(v2[j]) != 0){
+                        return -1;
+                    }
+                }
+                return 0;
+            }else if(i >= v2.length){
+                for(int j = i; j < length; j++){
+                    if(Integer.parseInt(v1[j]) != 0){
+                        return 1;
+                    }
+                }
+                return 0;
+            }else {
+                if(Integer.parseInt(v1[i]) > Integer.parseInt(v2[i])){
+                    return 1;
+                }else if(Integer.parseInt(v1[i]) < Integer.parseInt(v2[i])){
+                    return -1;
+                }
+            }
+        }
+        return 0;
     }
 }

27e8b97fb59ca8b21e800ee93ffeb3581b53e4d5
FisherBi <1015329590@qq.com>
2016-07-19 23:40:15 +0800

    Valid Palindrome.

---
+++ src/algorithms/ValidPalindrome/java/ValidPalindrome.java
@@ -0,0 +1,10 @@
+package algorithms.ValidPalindrome.java;
+
+/**
+ * Created by FisherBi on 2016/7/19.
+ */
+public class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        return true;
+    }
+}

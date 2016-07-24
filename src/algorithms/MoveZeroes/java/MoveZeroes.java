7b14b85cc64d69b56a68640086229feb47e24458
FisherBi <1015329590@qq.com>
2016-07-24 22:45:43 +0800

    moveZeroes

--- src/algorithms/MoveZeroes/java/MoveZeroes.java
+++ src/algorithms/MoveZeroes/java/MoveZeroes.java
@@ -5,6 +5,18 @@ package algorithms.MoveZeroes.java;
  */
 public class MoveZeroes {
     public static void moveZeroes(int[] nums) {
-
+        int i = 0;
+        int j = 0;
+        while (i < nums.length) {
+            if (nums[i] != 0) {
+                if (i != j) {
+                    nums[j++] = nums[i];
+                    nums[j] = 0;
+                }else {
+                    i++;
+                }
+            }
+            i++;
+        }
     }
 }

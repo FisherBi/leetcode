c2508c936115dd50422c7ed1109a1c494c81b120
fisbii <fisher.bi@leandev.se>
2016-09-14 13:51:04 +0800

    count and say.

--- src/CountAndSay/Solution.java
+++ src/CountAndSay/Solution.java
@@ -6,9 +6,9 @@ package CountAndSay;
 public class Solution {
     public static String countAndSay(int n) {
         String seq = "1";
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i < n; i++) {
             StringBuilder res = new StringBuilder();
-//            boolean flag = false;
+            boolean flag = false;
             int count = 1;
             char tmp = seq.charAt(0);
             for(int j = 1; j < seq.length(); j++){
@@ -16,16 +16,18 @@ public class Solution {
                     count++;
                 }else{
                     res.append(count).append(tmp);
-                    count = 1;
+                    tmp = seq.charAt(j);
+                    count=1;
                 }
             }
+            res.append(count).append(tmp);
             seq = res.toString();
         }
         return seq;
     }
 
     public static void main(String args[]){
-        for(int i = 2; i <= 6; i++){
+        for(int i = 1; i <= 6; i++){
             System.out.println(countAndSay(i));
         }
 
